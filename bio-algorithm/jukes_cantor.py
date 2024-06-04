6962c64df041ad6b71039c71e89baa241ccbb1dc
Sitao Zhu <zhusitao1990@163.com>
2024-06-04 10:38:09 +0800

    Update jukes_cantor.py

--- bio-algorithm/jukes_cantor.py
+++ bio-algorithm/jukes_cantor.py
@@ -3,16 +3,15 @@ import re math
 # Jukes-Cantor Distance on sequences
 
 def distance(seq1, seq2):
-  # Jukes Cantor distance formula: (-3/4)ln[1-p*(4/3)]
-  p = percent_difference_of_nucleotides(seq1, seq2)
-  return -0.75 * math.log(1 - (p*4/3)) if p else 0
+	# Jukes Cantor distance formula: (-3/4)ln[1-p*(4/3)]
+	p = percent_difference_of_nucleotides(seq1, seq2)
+	return -0.75 * math.log(1 - (p*4/3)) if p else 0
 
 
 def percent_difference_of_nucleotides (seq1, seq2, nucleobases=set('ACGT')):
-  # percentage of nucleotide difference in two sequences
-  diff_count = 0 # number of nucleotide differences
+	# percentage of nucleotide difference in two sequences
+	diff_count = 0 # number of nucleotide differences
 	valid_nucleotides_count = 0.0 # number of valid nucleotides (value is float for computing percentage)
-
 	for a, b in zip(seq1, seq2):
 		if a in nucleobases and b in nucleobases:
 			valid_nucleotides_count += 1
